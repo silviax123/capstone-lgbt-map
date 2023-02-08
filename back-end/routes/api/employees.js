20094099d510e9975d6911175075ed99259a73eb
Silvia <sx0771@gmail.com>
2023-02-08 14:24:01 -0500

    clean up

--- back-end/routes/api/employees.js
+++ back-end/routes/api/employees.js
@@ -1,33 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const data = {};
-data.employees = require('../../data/employees.json');
+const employeesController = require('../../controllers/employeesController')
 
 router.route('/')
-    .get((req, res) => {
-        res.json(data.employees);
-    })
-    .post((req, res) => {
-        res.json({
-            "firstname": req.body.firstname,
-            "lastname": req.body.lastname
-        })
-    })
-    .put((req,res) => {
-        res.json({
-            "firstname": req.body.firstname,
-            "lastname": req.body.lastname
-        })
-    })
-    .delete((req, res) => {
-        res.json({"id": req.body,id})
-    });
+    .get(employeesController.getAllEmployees)
+    .post(employeesController.createNewEmployee)
+    .put(employeesController.updateEmployee)
+    .delete(employeesController.deleteEmployee);
 
 //:id is a named parameter and we're pulling it directly out of the url
 // so we are responding with what we received 
-    router.route('/:id')
-    .get((req, res) => {
-        res.json({ "id": req.params.id })
-    });
+router.route('/:id')
+    .get(employeesController.getEmployee);
 
 module.exports = router;
